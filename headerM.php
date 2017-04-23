1f32ccd28d2ffaaab04ff712df57d700c7da6c98
SAUL\Saul_Manich <saulmamani73@gmail.com>
2017-04-23 15:54:44 +0200

    Admin

---
+++ headerM.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Admin</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="view/libs/css/bootstrap.min.css">
+    <link rel="stylesheet" href="view/libs/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="view/libs/css/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="view/libs/css/estiloResp.css">
+    
+    
+    </head>
+    <body>
+        
